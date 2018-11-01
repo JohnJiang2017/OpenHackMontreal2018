290a641079c2fd77ff26e94f8040f10b7a50abdd
Haipeng Jiang <johnhappen@hotmail.com>
2018-11-01 14:17:36 -0400

    changed a bit more

--- HttpTrigger/index.js
+++ HttpTrigger/index.js
@@ -4,7 +4,7 @@ module.exports = async function (context, req) {
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello yes man " + (req.query.name || req.body.name)
+            body: "Hello yes man and again" + (req.query.name || req.body.name)
         };
     }
     else {
@@ -13,4 +13,4 @@ module.exports = async function (context, req) {
             body: "Please pass a name on the query string or in the request body"
         };
     }
-};
+};  
